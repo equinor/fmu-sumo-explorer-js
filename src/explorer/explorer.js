cd69eafaa2a3c1ecc04e6056c201bbbef0885d98
Raymond Wiker <rayw@equinor.com>
2025-11-25 10:33:11 +0100

    Cleanup.

--- src/explorer/explorer.js
+++ src/explorer/explorer.js
@@ -1,20 +1,4 @@
-import SearchContext from "./search-context.js";
-
-import Case from "./case.js";
-import Ensemble from "./ensemble.js";
-import Realization from "./realization.js";
-
-import Cases from "./cases.js";
-import Ensembles from "./ensembles.js";
-import Realizations from "./realizations.js";
-
-import CPGrid from "./cpgrid.js";
-import CPGridProperty from "./cpgrid-property.js";
-import Cube from "./cube.js";
-import Dictionary from "./dictionary.js";
-import Polygons from "./polygons.js";
-import Surface from "./surface.js";
-import Table from "./table.js";
+import SearchContext from "./objects/search-context.js";
 
 class Explorer extends SearchContext {
   constructor(sumo) {
@@ -22,18 +6,4 @@ class Explorer extends SearchContext {
   }
 }
 
-export {
-  Case,
-  Ensemble,
-  Realization,
-  Cases,
-  Ensembles,
-  Realizations,
-  CPGrid,
-  CPGridProperty,
-  Cube,
-  Dictionary,
-  Polygons,
-  Surface,
-  Table,
-};
+export default Explorer;
